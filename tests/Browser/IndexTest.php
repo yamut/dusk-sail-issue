9688cdd2d99c5e68323b39c7c290295aa3e48363
Dan Mattern <dan@danmattern.com>
2024-01-19 15:08:23 -0500

    Initial commit

---
+++ tests/Browser/IndexTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class IndexTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            throw new \RuntimeException('foo');
+            $browser->visit('/')
+                    ->assertSee('Laravel');
+        });
+    }
+}
